189aed38704daad7dce0f4c07550fdf4a8b6a253
Shengying Wang <swang10@berkeley.edu>
2018-03-19 11:40:59 -0700

    update

--- data_structures.py
+++ data_structures.py
@@ -234,4 +234,196 @@ for date, stop, riders in entries:
     ridership[stop].append(riders)
 
 # Print the first 10 items of the ridership dictionary
-print(list(ridership.items())[:10])     
+print(list(ridership.items())[:10])
+
+#ex4
+# Import OrderedDict from collections
+from collections import OrderedDict
+
+# Create an OrderedDict called: ridership_date
+ridership_date = OrderedDict()
+
+# Iterate over the entries
+for date, riders in entries:
+    # If a key does not exist in ridership_date, set it to 0
+    if not date in ridership_date:
+        ridership_date[date] = 0
+
+    # Add riders to the date key in ridership_date
+    ridership_date[date] += riders
+
+# Print the first 31 records
+print(list(ridership_date.items())[:31])
+
+# Print the first key in ridership_date
+print(list(ridership_date.keys())[0])
+
+# Pop the first item from ridership_date and print it
+print(ridership_date.popitem(last=False))
+
+# Print the last key in ridership_date
+print(list(ridership_date.keys())[-1])
+
+# Pop the last item from ridership_date and print it
+print(ridership_date.popitem())
+
+#ex6
+# Import namedtuple from collections
+from collections import namedtuple
+
+# Create the namedtuple: DateDetails
+DateDetails = namedtuple('DateDetails', ['date', 'stop', 'riders'])
+
+# Create the empty list: labeled_entries
+labeled_entries = []
+
+# Iterate over the entries
+for date, stop, riders in entries:
+    # Append a new DateDetails namedtuple instance for each entry to labeled_entries
+    labeled_entries.append(DateDetails(date, stop, riders))
+
+# Print the first 5 items in labeled_entries
+print(labeled_entries[:5])
+
+# Iterate over the first twenty items in labeled_entries
+for item in labeled_entries[:20]:
+    # Print each item's stop
+    print(item.stop)
+
+    # Print each item's date
+    print(item.date)
+
+    # Print each item's riders
+    print(item.riders)
+
+#Chapter 4
+#ex1
+# Import the datetime object from datetime
+from datetime import datetime
+
+# Iterate over the dates_list
+for date_str in dates_list:
+    # Convert each date to a datetime object: date_dt
+    date_dt = datetime.strptime(date_str, '%m/%d/%Y')
+
+    # Print each date_dt
+    print(date_dt)
+
+# Loop over the first 10 items of the datetimes_list
+for item in datetimes_list[:10]:
+    # Print out the record as a string in the format of 'MM/DD/YYYY'
+    print(item.strftime('%m/%d/%Y'))
+
+    # Print out the record as an ISO standard string
+    print(item.isoformat())
+
+#ex2
+# Create a defaultdict of an integer: monthly_total_rides
+monthly_total_rides = defaultdict(int)
+
+# Loop over the list daily_summaries
+for daily_summary in daily_summaries:
+    # Convert the service_date to a datetime object
+    service_datetime = datetime.strptime(daily_summary[0], '%m/%d/%Y')
+
+    # Add the total rides to the current amount for the month
+    monthly_total_rides[service_datetime.month] += int(daily_summary[4])
+
+# Print monthly_total_rides
+print(monthly_total_rides)
+
+#ex3
+# Import datetime from the datetime module
+from datetime import datetime
+
+# Compute the local datetime: local_dt
+local_dt = datetime.now()
+
+# Print the local datetime
+print(local_dt)
+
+# Compute the UTC datetime: utc_dt
+utc_dt = datetime.utcnow()
+
+# Print the UTC datetime
+print(utc_dt)
+
+# Create a Timezone object for Chicago
+chicago_usa_tz = timezone('US/Central')
+
+# Create a Timezone object for New York
+ny_usa_tz = timezone('US/Eastern')
+
+# Iterate over the daily_summaries list
+for orig_dt, ridership in daily_summaries:
+
+    # Make the orig_dt timezone "aware" for Chicago
+    chicago_dt = orig_dt.replace(tzinfo=chicago_usa_tz)
+
+    # Convert chicago_dt to the New York Timezone
+    ny_dt = chicago_dt.astimezone(ny_usa_tz)
+
+    # Print the chicago_dt, ny_dt, and ridership
+    print('Chicago: %s, NY: %s, Ridership: %s' % (chicago_dt, ny_dt, ridership))
+
+#ex5
+# Import timedelta from the datetime module
+from datetime import timedelta
+
+# Build a timedelta of 30 days: glanceback
+glanceback = timedelta(days=30)
+
+# Iterate over the review_dates as date
+for date in review_dates:
+    # Calculate the date 30 days back: prior_period_dt
+    prior_period_dt = date - glanceback
+
+    # Print the review_date, day_type and total_ridership
+    print('Date: %s, Type: %s, Total Ridership: %s' %
+         (date,
+          daily_summaries[date]['day_type'],
+          daily_summaries[date]['total_ridership']))
+
+    # Print the prior_period_dt, day_type and total_ridership
+    print('Date: %s, Type: %s, Total Ridership: %s' %
+         (prior_period_dt,
+          daily_summaries[prior_period_dt]['day_type'],
+          daily_summaries[prior_period_dt]['total_ridership']))
+
+# Iterate over the date_ranges
+for start_date, end_date in date_ranges:
+    # Print the End and Start Date
+    print(end_date, start_date)
+    # Print the difference between each end and start date
+    print(end_date - start_date)
+
+#ex6
+# Import the pendulum module
+import pendulum
+
+# Create a now datetime for Tokyo: tokyo_dt
+tokyo_dt = pendulum.now('Asia/Tokyo')
+
+# Covert the tokyo_dt to Los Angeles: la_dt
+la_dt = tokyo_dt.in_timezone('America/Los_Angeles')
+
+# Print the ISO 8601 string of la_dt
+print(la_dt.to_iso8601_string())
+
+# Iterate over date_ranges
+for start_date, end_date in date_ranges:
+
+    # Convert the start_date string to a pendulum date: start_dt
+    start_dt = pendulum.parse(start_date)
+
+    # Convert the end_date string to a pendulum date: end_dt
+    end_dt = pendulum.parse(end_date)
+
+    # Print the End and Start Date
+    print(end_dt, start_dt)
+
+    # Calculate the difference between end_dt and start_dt: diff_period
+    diff_period = end_dt - start_dt
+
+    # Print the difference in days
+    print(diff_period.in_days())
--- visualizations.py
+++ visualizations.py
@@ -385,3 +385,417 @@ plt.axis('off')
 plt.imshow(rescaled_image)
 
 plt.show()
+
+#Chapter 3
+#ex1: Linear Regression Plot
+# Import plotting modules
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Plot a linear regression between 'weight' and 'hp'
+sns.lmplot(x='weight', y='hp', data=auto)
+
+# Display the plot
+plt.show()
+
+# Import plotting modules
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Generate a green residual plot of the regression between 'hp' and 'mpg'
+sns.residplot(x='hp', y='mpg', data=auto, color='green')
+
+# Display the plot
+plt.show()
+
+# Generate a scatter plot of 'weight' and 'mpg' using red circles
+plt.scatter(auto['weight'], auto['mpg'], label='data', color='red', marker='o')
+
+# Plot in blue a linear regression of order 1 between 'weight' and 'mpg'
+sns.regplot(x='weight', y='mpg', data=auto, scatter=None, color='blue', label='order 1')
+
+# Plot in green a linear regression of order 2 between 'weight' and 'mpg'
+sns.regplot(x='weight', y='mpg', data=auto, scatter=None, color='green', label='order 2',order=2)
+
+# Add a legend and display the plot
+plt.legend(loc='upper right')
+plt.show()
+
+# Plot a linear regression between 'weight' and 'hp', with a hue of 'origin' and palette of 'Set1'
+sns.lmplot(x='weight',y='hp',data=auto,hue='origin',palette='Set1')
+
+# Display the plot
+plt.show()
+
+# Plot linear regressions between 'weight' and 'hp' grouped row-wise by 'origin'
+sns.lmplot(x='weight',y='hp',data=auto,row='origin',palette='Set1')
+
+# Display the plot
+plt.show()
+
+#ex2: Distribution Plot
+# Make a strip plot of 'hp' grouped by 'cyl'
+plt.subplot(2,1,1)
+sns.stripplot(x='cyl', y='hp', data=auto)
+
+# Make the strip plot again using jitter and a smaller point size
+plt.subplot(2,1,2)
+sns.stripplot(x='cyl', y='hp', data=auto, jitter=True, size=3)
+
+# Display the plot
+plt.show()
+
+# Generate a swarm plot of 'hp' grouped horizontally by 'cyl'
+plt.subplot(2,1,1)
+sns.swarmplot(x='cyl', y='hp', data=auto)
+
+# Generate a swarm plot of 'hp' grouped vertically by 'cyl' with a hue of 'origin'
+plt.subplot(2,1,2)
+sns.swarmplot(y='cyl', x='hp', data=auto, hue='origin', orient='h')
+
+# Display the plot
+plt.show()
+
+# Generate a violin plot of 'hp' grouped horizontally by 'cyl'
+plt.subplot(2,1,1)
+sns.violinplot(x='cyl', y='hp', data=auto)
+
+# Generate the same violin plot again with a color of 'lightgray' and without inner annotations
+plt.subplot(2,1,2)
+sns.violinplot(x='cyl', y='hp', data=auto, inner=None, color='lightgray')
+
+# Overlay a strip plot on the violin plot
+sns.stripplot(x='cyl', y='hp', data=auto, jitter=True, size=1.5)
+
+# Display the plot
+plt.show()
+
+#ex3: Multivariate Distribution Plot
+# Generate a joint plot of 'hp' and 'mpg'
+sns.jointplot(x='hp', y='mpg', data=auto)
+
+# Display the plot
+plt.show()
+
+# Generate a joint plot of 'hp' and 'mpg' using a hexbin plot
+sns.jointplot(x='hp', y='mpg', data=auto, kind='hex')
+
+# Display the plot
+plt.show()
+
+# Print the first 5 rows of the DataFrame
+print(auto.head())
+
+# Plot the pairwise joint distributions from the DataFrame
+sns.pairplot(auto)
+
+# Display the plot
+plt.show()
+
+# Print the first 5 rows of the DataFrame
+print(auto.head())
+
+# Plot the pairwise joint distributions grouped by 'origin' along with regression lines
+sns.pairplot(auto, kind='reg', hue='origin')
+
+# Display the plot
+plt.show()
+
+# Print the covariance matrix
+print(cov_matrix)
+
+# Visualize the covariance matrix using a heatmap
+sns.heatmap(cov_matrix)
+
+# Display the heatmap
+plt.show()
+
+#Chapter 4
+#ex1: Time Series
+# Import matplotlib.pyplot
+import matplotlib.pyplot as plt
+
+# Plot the aapl time series in blue
+plt.plot(aapl, color='blue', label='AAPL')
+
+# Plot the ibm time series in green
+plt.plot(ibm, color='green', label='IBM')
+
+# Plot the csco time series in red
+plt.plot(csco, color='red', label='CSCO')
+
+# Plot the msft time series in magenta
+plt.plot(msft, color='magenta', label='MSFT')
+
+# Add a legend in the top left corner of the plot
+plt.legend(loc='upper left')
+
+# Specify the orientation of the xticks
+plt.xticks(rotation=60)
+
+# Display the plot
+plt.show()
+
+# Plot the series in the top subplot in blue
+plt.subplot(2,1,1)
+plt.xticks(rotation=45)
+plt.title('AAPL: 2001 to 2011')
+plt.plot(aapl, color='blue')
+
+# Slice aapl from '2007' to '2008' inclusive: view
+view = aapl['2007':'2008']
+
+# Plot the sliced data in the bottom subplot in black
+plt.subplot(2,1,2)
+plt.xticks(rotation=45)
+plt.title('AAPL: 2007 to 2008')
+plt.plot(view, color='black')
+plt.tight_layout()
+plt.show()
+
+# Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
+view = aapl['2007-11':'2008-04']
+
+# Plot the sliced series in the top subplot in red
+plt.subplot(2,1,1)
+plt.xticks(rotation=45)
+plt.title('AAPL: Nov. 2007 to Apr. 2008')
+plt.plot(view, color='red')
+
+# Reassign the series by slicing the month January 2008: view
+view = aapl['2008-01']
+
+# Plot the sliced series in the bottom subplot in green
+plt.subplot(2,1,2)
+plt.xticks(rotation=45)
+plt.title('AAPL: Jan. 2008')
+plt.plot(view, color='green')
+
+# Improve spacing and display the plot
+plt.tight_layout()
+plt.show()
+
+# Slice aapl from Nov. 2007 to Apr. 2008 inclusive: view
+view = aapl['2007-11':'2008-04']
+
+# Plot the entire series
+plt.plot(aapl)
+plt.xticks(rotation=45)
+plt.title('AAPL: 2001-2011')
+
+# Specify the axes
+plt.axes([0.25, 0.5, 0.35, 0.35])
+
+# Plot the sliced series in red using the current axes
+plt.plot(view, color='red')
+plt.xticks(rotation=45)
+plt.title('2007/11-2008/04')
+plt.show()
+
+#ex2: Window Averages
+# Plot the 30-day moving average in the top left subplot in green
+plt.subplot(2, 2, 1)
+plt.plot(mean_30, 'green')
+plt.plot(aapl, 'k-.')
+plt.xticks(rotation=60)
+plt.title('30d averages')
+
+# Plot the 75-day moving average in the top right subplot in red
+plt.subplot(2, 2, 2)
+plt.plot(mean_75, 'red')
+plt.plot(aapl, 'k-.')
+plt.xticks(rotation=60)
+plt.title('75d averages')
+
+# Plot the 125-day moving average in the bottom left subplot in magenta
+plt.subplot(2, 2, 3)
+plt.plot(mean_125, 'magenta')
+plt.plot(aapl, 'k-.')
+plt.xticks(rotation=60)
+plt.title('125d averages')
+
+# Plot the 250-day moving average in the bottom right subplot in cyan
+plt.subplot(2, 2, 4)
+plt.plot(mean_250, 'cyan')
+plt.plot(aapl, 'k-.')
+plt.xticks(rotation=60)
+plt.title('250d averages')
+
+# Display the plot
+plt.show()
+
+# Plot std_30 in red
+plt.plot(std_30, 'red', label='30d')
+
+# Plot std_75 in cyan
+plt.plot(std_75, 'cyan', label='75d')
+
+# Plot std_125 in green
+plt.plot(std_125, 'green', label='125d')
+
+# Plot std_250 in magenta
+plt.plot(std_250, 'magenta', label='250d')
+
+# Add a legend to the upper left
+plt.legend(loc='upper left')
+
+# Add a title
+plt.title('Moving standard deviations')
+
+# Display the plot
+plt.show()
+
+#ex3: Histogram Equalization
+# Load the image into an array: image
+image = plt.imread('640px-Unequalized_Hawkes_Bay_NZ.jpg')
+
+# Display image in top subplot using color map 'gray'
+plt.subplot(2,1,1)
+plt.title('Original image')
+plt.axis('off')
+plt.imshow(image, cmap='gray')
+
+# Assign pixels the flattened 1D numpy array image.flatten()
+pixels = image.flatten()
+
+# Display a histogram of the pixels in the bottom subplot
+plt.subplot(2,1,2)
+plt.xlim((0,255))
+plt.title('Normalized histogram')
+plt.hist(pixels, bins=64, color='red', alpha=0.4, range=(0,256), normed=True)
+plt.show()
+
+# Load the image into an array: image
+image = plt.imread('640px-Unequalized_Hawkes_Bay_NZ.jpg')
+
+# Display image in top subplot using color map 'gray'
+plt.subplot(2,1,1)
+plt.imshow(image, cmap='gray')
+plt.title('Original image')
+plt.axis('off')
+
+# Flatten the image into 1 dimension: pixels
+pixels = image.flatten()
+
+# Display a histogram of the pixels in the bottom subplot
+plt.subplot(2,1,2)
+pdf = plt.hist(pixels, bins=64, range=(0,256), normed=False,
+               color='red', alpha=0.4)
+plt.grid('off')
+
+# Use plt.twinx() to overlay the CDF in the bottom subplot
+plt.twinx()
+
+# Display a cumulative histogram of the pixels
+cdf = plt.hist(pixels, bins=64, range=(0,256),
+               cumulative=True, normed=True,
+               color='blue', alpha=0.4)
+
+# Specify x-axis range, hide axes, add title and display plot
+plt.xlim((0,256))
+plt.grid('off')
+plt.title('PDF & CDF (original image)')
+plt.show()
+
+# Load the image into an array: image
+image = plt.imread('640px-Unequalized_Hawkes_Bay_NZ.jpg')
+
+# Flatten the image into 1 dimension: pixels
+pixels = image.flatten()
+
+# Generate a cumulative histogram
+cdf, bins, patches = plt.hist(pixels, bins=256, range=(0,256), normed=True, cumulative=True)
+new_pixels = np.interp(pixels, bins[:-1], cdf*255)
+
+# Reshape new_pixels as a 2-D array: new_image
+new_image = new_pixels.reshape(image.shape)
+
+# Display the new image with 'gray' color map
+plt.subplot(2,1,1)
+plt.title('Equalized image')
+plt.axis('off')
+plt.imshow(new_image, cmap='gray')
+
+# Generate a histogram of the new pixels
+plt.subplot(2,1,2)
+pdf = plt.hist(new_pixels, bins=64, range=(0,256), normed=False,
+               color='red', alpha=0.4)
+plt.grid('off')
+
+# Use plt.twinx() to overlay the CDF in the bottom subplot
+plt.twinx()
+plt.xlim((0,256))
+plt.grid('off')
+
+# Add title
+plt.title('PDF & CDF (equalized image)')
+
+# Generate a cumulative histogram of the new pixels
+cdf = plt.hist(new_pixels, bins=64, range=(0,256),
+               cumulative=True, normed=True,
+               color='blue', alpha=0.4)
+plt.show()
+
+# Load the image into an array: image
+image = plt.imread('hs-2004-32-b-small_web.jpg')
+
+# Display image in top subplot
+plt.subplot(2,1,1)
+plt.title('Original image')
+plt.axis('off')
+plt.imshow(image)
+
+# Extract 2-D arrays of the RGB channels: red, blue, green
+red, blue, green = image[:,:,0], image[:,:,1], image[:,:,2]
+
+# Flatten the 2-D arrays of the RGB channels into 1-D
+red_pixels = red.flatten()
+blue_pixels = blue.flatten()
+green_pixels = green.flatten()
+
+# Overlay histograms of the pixels of each color in the bottom subplot
+plt.subplot(2,1,2)
+plt.title('Histograms from color image')
+plt.xlim((0,256))
+plt.hist(red_pixels, bins=64, normed=True, color='red', alpha=0.2)
+plt.hist(blue_pixels, bins=64, normed=True, color='blue', alpha=0.2)
+plt.hist(green_pixels, bins=64, normed=True, color='green', alpha=0.2)
+
+# Display the plot
+plt.show()
+
+# Load the image into an array: image
+image = plt.imread('hs-2004-32-b-small_web.jpg')
+
+# Extract RGB channels and flatten into 1-D array
+red, blue, green = image[:,:,0], image[:,:,1], image[:,:,2]
+red_pixels = red.flatten()
+blue_pixels = blue.flatten()
+green_pixels = green.flatten()
+
+# Generate a 2-D histogram of the red and green pixels
+plt.subplot(2,2,1)
+plt.grid('off')
+plt.xticks(rotation=60)
+plt.xlabel('red')
+plt.ylabel('green')
+plt.hist2d(red_pixels, green_pixels, bins=(32,32))
+
+# Generate a 2-D histogram of the green and blue pixels
+plt.subplot(2,2,2)
+plt.grid('off')
+plt.xticks(rotation=60)
+plt.xlabel('green')
+plt.ylabel('blue')
+plt.hist2d(green_pixels, blue_pixels, bins=(32, 32))
+
+# Generate a 2-D histogram of the blue and red pixels
+plt.subplot(2,2,3)
+plt.grid('off')
+plt.xticks(rotation=60)
+plt.xlabel('blue')
+plt.ylabel('red')
+plt.hist2d(blue_pixels, red_pixels, bins=(32, 32))
+
+# Display the plot
+plt.show()
